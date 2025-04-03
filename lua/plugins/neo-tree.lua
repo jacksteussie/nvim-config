c4b046d49ef42a531846817867d2ba4529c48501
Jack Steussie <jack.r.steussie.ctr@us.navy.mil>
2025-04-03 11:49:19 -0700

    added neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      mappings = {
+        ["v"] = "open_vsplit",
+        ["s"] = "open_split",
+        ["t"] = "open_tabnew",
+      },
+    },
+  },
+}
